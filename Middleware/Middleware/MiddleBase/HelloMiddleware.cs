77d7f4fbe0a3138b3efc4b49f8eaf6c859bc2bea
Fatmaaktar <fatmaaktar214@gmail.com>
2023-10-22 22:12:21 +0300

    Asp.net-Core-MVC

---
+++ Middleware/Middleware/MiddleBase/HelloMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Midddleware.MiddleBase
+{
+    public class HelloMiddleware
+    {
+        RequestDelegate _next;
+        public HelloMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            //Custom Operasyon...
+            Console.WriteLine("Hello");
+            await _next.Invoke(httpContext);
+            Console.WriteLine("Hello");
+        }
+    }
+}
